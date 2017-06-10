5a1adce9013172516f726999b878652261636995
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-06-10 22:23:17 +0300

    Add object_model functionality

--- lib/topics/object_model.rb
+++ lib/topics/object_model.rb
@@ -2,4 +2,30 @@
 
 # exercise object model
 module ObjectModel
+  #
+  module LinuxFriendly
+    def fork_process
+      super
+    end
+  end
+  #
+  class PersonalComputer
+    def fork_process
+      fork do
+        puts "We created new child process with pid #{Process.pid}"
+      end
+      'Parent: allocate memory'
+    end
+
+    def mine_bitcoins
+      # My attempt to make random hash digest creation
+      num_of_hexidecimals = 20
+      puts Array.new(num_of_hexidecimals) { format('%02X', rand(0..255)) }.join
+      inspect
+    end
+  end
+  #
+  class Laptop < PersonalComputer
+    prepend LinuxFriendly
+  end
 end
